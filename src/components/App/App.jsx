5f95a1b3a23952937d0ae47b9d3627efb262d070
Caio Mendes <caiosamarone10@hotmail.com>
2020-09-12 13:40:13 -0300

    add Line charts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import  AppHeader  from '../AppHeader/'
 import { Wrapper, Container } from './App.styles'
 import AppContainer from '../AppContainer'
 import Checkbox from '../../shared/Checkbox/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 
 
 function App (){
     const[lettuce,setLettuce] = useState() /**faz um get e um set para mudar o estado de uma propriedade */
+    
+    const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+
+ 
+   
 
 
     return <Wrapper>
@@ -21,17 +27,28 @@ function App (){
                         title="Alface"
                         onClick={() => setLettuce(!lettuce)}
                     />
-                    <Checkbox
-                        value={false}
-                        title="Mandioca"
-                    />
+                        <Checkbox
+                            value={false}
+                            title="Mandioca"
+                        />
                     
               </div>}
               middle={<div>
                   sua lista de compras
+
+                  <Checkbox
+                        value={false}
+                        title="Mandioca"
+                 />
               </div>}
               right={<div>
                     estatisticas
+
+                    <LineChart  color={colors[0]}  title=" Saudavel" percentage = {30}    />
+                    <LineChart  color={colors[1]}  title=" Nao tao saudavel" percentage = {40}    />
+                    <LineChart  color={colors[2]}  title=" Limpeza" percentage = {70}    />
+                    <LineChart  color={colors[3]}  title=" Outros" percentage = {10}    />
+                    
               </div>}
             />
         </Container>
--- src/shared/LineChart/LineChart.jsx
+++ src/shared/LineChart/LineChart.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { ProgressBar, Wrapper } from './LineChart.styles'
 
-function LineChat (){
-    
+function LineChat ({ title, percentage, color }){
+    return <Wrapper>
+        <span>
+            { title }:
+        </span>
+        <ProgressBar 
+            percentage={percentage}
+            color={color} 
+        />
+    </Wrapper>
 }
+
+export default LineChat
